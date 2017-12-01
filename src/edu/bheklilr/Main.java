eff535554b36ce1f277eab00c2ce736c01ef3734
Aaron Stevens <bheklilr2@gmail.com>
2017-12-01 13:27:09 -0600

    Implemented Day 1

---
+++ src/edu/bheklilr/Main.java
@@ -0,0 +1,13 @@
+package edu.bheklilr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int millisecond = 0;
+
+        Solution[] solutions = new Solution[] {
+	            new Solution1(),
+        };
+	    solutions[millisecond].solve();
+    }
+}
---
+++ src/edu/bheklilr/Solution.java
@@ -0,0 +1,5 @@
+package edu.bheklilr;
+
+public interface Solution {
+    void solve();
+}
