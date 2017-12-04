a4f5cc3db4ea7da947188fa881b5d590ea4c9683
Aaron Stevens <bheklilr2@gmail.com>
2017-12-04 09:28:47 -0600

    Finished part 2

--- src/edu/bheklilr/Solution3.java
+++ src/edu/bheklilr/Solution3.java
@@ -1,5 +1,12 @@
 package edu.bheklilr;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Solution3 implements Solution {
 
     private static final int INPUT = 265149;
@@ -10,11 +17,42 @@ public class Solution3 implements Solution {
         System.out.println("Part 2: " + solvePart2());
     }
 
-    private int solvePart2() {
-        int sideLength = (int) Math.ceil(Math.sqrt((double) INPUT));
-        if (sideLength % 2 == 0) {
-            sideLength++;
+    private class Pair<L, R> {
+        private final L left;
+        private final R right;
+
+        public Pair(L left, R right) {
+            this.left = left;
+            this.right = right;
         }
+
+        public L getLeft() {
+            return left;
+        }
+
+        public R getRight() {
+            return right;
+        }
+
+        @Override
+        public int hashCode() {
+            return left.hashCode() ^ right.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pair)) return false;
+            Pair other = (Pair) o;
+            return getLeft() == other.getLeft() && getRight() == other.getRight();
+        }
+    }
+
+    private int solvePart2() {
+        int _sideLength = (int) Math.ceil(Math.sqrt((double) INPUT));
+        if (_sideLength % 2 == 0) {
+            _sideLength++;
+        }
+        final int sideLength = _sideLength;
         int[][] grid = new int[sideLength][sideLength];
         for (int i = 0; i < sideLength; i++) {
             for (int j = 0; j < sideLength; j++) {
@@ -23,21 +61,82 @@ public class Solution3 implements Solution {
         }
 
         // (0, 0) is grid[0][0], so we're starting in the middle
-        int x = sideLength / 2;
-        int y = x;
+        final int origin = sideLength / 2;
+        int x = origin;
+        int y = origin;
 
         grid[x][y] = 1;
+        x++;
 
-        char direction = 'r';
-        int currSideLength = 1;
-
+        char direction = 'u';
         int lastValue = 1;
 
-        while (lastValue < INPUT) {
-            // TODO: The rest of the problem
-        }
+        Function<Integer, Integer> clip =
+                _x -> Math.max(Math.min(_x, sideLength - 1), 0);
+        BiFunction<Integer, Integer, Pair<Integer, Integer>> mkpair =
+                (_x, _y) -> new Pair<>(clip.apply(_x), clip.apply(_y));
 
+        while (lastValue < INPUT) {
+            // Determine the sum at (x, y)
+            int localSum = getLocalSum(grid, getAdjacentIdxs(x, y, mkpair));
+            grid[x][y] = localSum;
+            lastValue = localSum;
+            System.out.format("%s (%d, %d): %d\n", direction, x - origin, y - origin, lastValue);
+
+            // Update the current position, checking if a direction change is needed
+            switch (direction) {
+                case 'u':
+                    if (grid[x - 1][y] == 0) direction = 'l';
+                    break;
+                case 'd':
+                    if (grid[x + 1][y] == 0) direction = 'r';
+                    break;
+                case 'l':
+                    if (grid[x][y - 1] == 0) direction = 'd';
+                    break;
+                case 'r':
+                    if (grid[x][y + 1] == 0) direction = 'u';
+                    break;
+            }
+            switch (direction) {
+                case 'u':
+                    y++;
+                    break;
+                case 'd':
+                    y--;
+                    break;
+                case 'l':
+                    x--;
+                    break;
+                case 'r':
+                    x++;
+                    break;
+            }
+        }
         return lastValue;
+    }
+
+    private int getLocalSum(int[][] grid, Set<Pair<Integer, Integer>> idxs) {
+        int localSum = 0;
+        for (Pair<Integer, Integer> idx : idxs) {
+            localSum += grid[idx.getLeft()][idx.getRight()];
+        }
+        return localSum;
+    }
+
+    @NotNull
+    private Set<Pair<Integer, Integer>> getAdjacentIdxs(int x, int y, BiFunction<Integer, Integer, Pair<Integer, Integer>> mkpair) {
+        Set<Pair<Integer, Integer>> idxs = new HashSet<>();
+        // This is about as much code as using 2 for loops
+        idxs.add(mkpair.apply(x - 1, y - 1));
+        idxs.add(mkpair.apply(x + 0, y - 1));
+        idxs.add(mkpair.apply(x + 1, y - 1));
+        idxs.add(mkpair.apply(x - 1, y + 0));
+        idxs.add(mkpair.apply(x + 1, y + 0));
+        idxs.add(mkpair.apply(x - 1, y + 1));
+        idxs.add(mkpair.apply(x + 0, y + 1));
+        idxs.add(mkpair.apply(x + 1, y + 1));
+        return idxs;
     }
 
     private int solvePart1() {
