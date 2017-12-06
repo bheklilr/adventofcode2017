1e3ecff6325641f24cc768e61f6a9bfc544b6d50
Aaron Stevens <bheklilr2@gmail.com>
2017-12-06 12:17:28 -0600

    At the suggestion of @adamclmns, switched to using reflection to dynamically load all solutions

--- src/main/java/edu/bheklilr/Main.java
+++ src/main/java/edu/bheklilr/Main.java
@@ -1,26 +1,25 @@
 package edu.bheklilr;
 
-import java.util.Calendar;
-import java.util.TimeZone;
+import org.reflections.Reflections;
+
+import java.util.*;
 
 public class Main {
 
-    private static final Solution[] SOLUTIONS = new Solution[25];
+    private static final Map<Integer, Solution> SOLUTIONS;
 
     static {
-        for (int day = 1; day <= 25; day++) {
-            SOLUTIONS[day - 1] = loadSolution(day);
+        SOLUTIONS = new HashMap<>();
+        Reflections reflections = new Reflections(Main.class.getPackage().getName());
+        Set<Class<? extends Solution>> solutions = reflections.getSubTypesOf(Solution.class);
+        for (Class<? extends Solution> s : solutions) {
+            int day = Integer.parseInt(s.getSimpleName().replace("Solution", ""));
+            try {
+                SOLUTIONS.put(day, s.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    private static Solution loadSolution(int day) {
-        String pkg = Main.class.getPackage().getName();
-        try {
-            return Solution.class.cast(Class.forName(String.format("%s.Solution%02d", pkg, day)).newInstance());
-        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     public static void main(String[] args) {
@@ -31,11 +30,13 @@ public class Main {
             // use current day
         }
         millisecond--;
-        if (millisecond < 0 || millisecond >= SOLUTIONS.length || SOLUTIONS[millisecond] == null) {
-            System.out.println("No solution for millisecond " + millisecond);
+        if (SOLUTIONS.containsKey(millisecond)) {
+            Solution s = SOLUTIONS.get(millisecond);
+            System.out.format("Solution for %dms\n", millisecond + 1);
+            System.out.println("  Part 1: " + s.solvePart1().toString());
+            System.out.println("  Part 2: " + s.solvePart2().toString());
         } else {
-            System.out.println("Part 1: " + SOLUTIONS[millisecond].solvePart1().toString());
-            System.out.println("Part 2: " + SOLUTIONS[millisecond].solvePart2().toString());
+            System.out.println("No solution for millisecond " + (millisecond + 1));
         }
     }
 }
