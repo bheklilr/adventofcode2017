00e819caae5b9be7107d3bc46ccc5289142786e3
Aaron Stevens <bheklilr2@gmail.com>
2017-12-07 14:00:47 -0600

    Added stub for Day 8

---
+++ src/main/java/edu/bheklilr/Solution08.java
@@ -0,0 +1,13 @@
+package edu.bheklilr;
+
+public class Solution08 implements Solution {
+    @Override
+    public Object solvePart1() {
+        return null;
+    }
+
+    @Override
+    public Object solvePart2() {
+        return null;
+    }
+}
