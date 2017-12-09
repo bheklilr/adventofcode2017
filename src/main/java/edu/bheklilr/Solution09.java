1668df9f7ab5dd7bf0bb7e15625c2b8f8ae233b3
Aaron Stevens <bheklilr2@gmail.com>
2017-12-09 16:11:50 -0600

    Implemented day 9.  Had some trouble because somehow some extra characters got inserted into the middle of my input string

---
+++ src/main/java/edu/bheklilr/Solution09.java
@@ -0,0 +1,79 @@
+package edu.bheklilr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Solution09 extends Solution<Long> {
+
+
+    @Override
+    Long solvePart1() {
+        try (InputStream stream = Files.newInputStream(Paths.get("inputs/09ms.txt"))) {
+            long score = 0;
+            long groupLevel = 0;
+            boolean inGarbage = false;
+
+            char b;
+            while (stream.available() > 0) {
+                b = (char) stream.read();
+                switch (b) {
+                    case '{':
+                        if (!inGarbage) {
+                            groupLevel++;
+                        }
+                        break;
+                    case '<':
+                        inGarbage = true;
+                        break;
+                    case '>':
+                        inGarbage = false;
+                        break;
+                    case '!':
+                        if (inGarbage) {
+                            // Increment the stream pointer
+                            //noinspection ResultOfMethodCallIgnored
+                            stream.read();
+                        }
+                        break;
+                    case '}':
+                        if (!inGarbage) {
+                            score += groupLevel;
+                            groupLevel -= 1;
+                        }
+                        break;
+                }
+            }
+            return score;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return (long) 0;
+        }
+    }
+
+    @Override
+    Long solvePart2() {
+        try (InputStream stream = Files.newInputStream(Paths.get("inputs/09ms.txt"))) {
+            long garbageChars = 0;
+            boolean inGarbage = false;
+            long score = 0;
+            long depth = 1;
+
+            char b;
+            while (stream.available() > 0) {
+                b = (char) stream.read();
+                if (b == '!') stream.read();
+                else if (inGarbage && b != '>') garbageChars++;
+                else if (b == '<') inGarbage = true;
+                else if (b == '>') inGarbage = false;
+                else if (b == '{') score += depth++;
+                else if (b == '}') depth--;
+            }
+            return garbageChars;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return (long) 0;
+        }
+    }
+}
