fc3e6dc132b4fa48d82d438ba6e40a4155577007
Aaron Stevens <bheklilr2@gmail.com>
2017-12-12 21:47:37 -0600

    Implemented Day 12.  Turned out to be over thinking things at first, ended up with pretty simple code I do think. The int[][] form of the graph turned out to be super useful, as did sets (as usual)

---
+++ src/main/java/edu/bheklilr/Solution12.java
@@ -0,0 +1,78 @@
+package edu.bheklilr;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution12 extends Solution<Integer> {
+
+    static Set<Integer> findConnected(Integer to, int[][] graph) {
+        Set<Integer> connected = new HashSet<>();
+        return findConnected(to, graph, connected);
+    }
+
+    static Set<Integer> findConnected(Integer to, int[][] graph, Set<Integer> connected) {
+        for (int i = 0; i < graph[to].length; i++) {
+            if (graph[to][i] == 1) {
+                List<Integer> scanNext = new ArrayList<>();
+                if (!connected.contains(i))
+                    scanNext.add(i);
+                connected.add(i);
+                for (Integer next : scanNext) {
+                    connected.addAll(findConnected(next, graph, connected));
+                }
+            }
+        }
+        return connected;
+    }
+
+    static int connectedNodes(Integer to, int[][] graph) {
+        return findConnected(to, graph).size();
+    }
+
+    Map<Integer, List<Integer>> getInputAssociations() {
+        return getInputLines("12ms")
+                .collect(Collectors.toMap(
+                        line -> Integer.parseInt(line.split(" <-> ")[0]),
+                        line -> {
+                            List<Integer> vals = new ArrayList<>();
+                            for (String n : line.split(" <-> ")[1].split(", ")) {
+                                vals.add(Integer.parseInt(n));
+                            }
+                            return vals;
+                        }
+                ));
+    }
+
+    int[][] getInputGraph() {
+        Map<Integer, List<Integer>> assocs = getInputAssociations();
+        int maxSize = Collections.max(assocs.keySet()) + 1;
+        int[][] graph = new int[maxSize][maxSize];
+        for (Map.Entry<Integer, List<Integer>> entry : assocs.entrySet()) {
+            for (Integer assoc : entry.getValue()) {
+                graph[entry.getKey()][assoc] = 1;
+            }
+            graph[entry.getKey()][entry.getKey()] = 1;
+        }
+        return graph;
+    }
+
+    @Override
+    Integer solvePart1() {
+        return connectedNodes(0, getInputGraph());
+    }
+
+    @Override
+    Integer solvePart2() {
+        int numGroups = 0;
+        int[][] graph = getInputGraph();
+        Set<Integer> nodesToSearch = new HashSet<>();
+        for (int i = 0; i < graph.length; i++) {
+            nodesToSearch.add(i);
+        }
+        while (nodesToSearch.size() > 0) {
+            nodesToSearch.removeAll(findConnected(nodesToSearch.iterator().next(), graph));
+            numGroups++;
+        }
+        return numGroups;
+    }
+}
